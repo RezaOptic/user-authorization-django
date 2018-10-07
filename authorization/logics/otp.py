6e3b17c9d4c53eb1f50627026ca620a0a395f9b7
Reza Noori <rezaoptic@yahoo.com>
2018-10-07 18:04:14 +0330

    add otp firewall

--- authorization/logics/otp.py
+++ authorization/logics/otp.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from authorization.logics.users import generate_token
-from helpers.redis_manager import get_from_redis, save_to_redis, save_to_redis_with_score, delete_redis_by_key
+from helpers.redis_manager import get_from_redis, save_to_redis, save_to_redis_with_score, delete_redis_by_key, \
+    get_from_redis_with_zrangebyscore
 from helpers.utils import create_sso_otp
 from services.otp import send_otp
 
@@ -45,3 +46,30 @@ def get_otp(phone_number, otp):
             delete_redis_by_key(phone_number)
             return True
     return False
+
+
+def check_access_phone_number_to_otp(phone_number):
+    _min = int((datetime.utcnow() - timedelta(days=7)).timestamp())
+    _max = int(datetime.utcnow().timestamp())
+    key = phone_number + ":" + "block"
+    value = get_from_redis_with_zrangebyscore(key, _min, "+inf")
+    if value and _max < int(value[-1].decode("utf-8")):
+        return False
+    return True
+
+
+def check_max_otp_request_and_block(phone_number):
+    _min = int((datetime.utcnow() - timedelta(minutes=2)).timestamp())
+    _max = int(datetime.utcnow().timestamp())
+    key = phone_number + ":" + "try"
+    value = get_from_redis_with_zrangebyscore(key, _min, _max)
+    if len(value) > 3:
+        key = phone_number + ":" + "block"
+        _min = int((datetime.utcnow() - timedelta(days=7)).timestamp())
+        value = get_from_redis_with_zrangebyscore(key, _min, "+inf")
+        minutes = 2 ** len(value)
+        score = int((datetime.utcnow() + timedelta(minutes=minutes)).timestamp())
+        save_to_redis_with_score(key, score, score)
+        return False
+    else:
+        return True
--- helpers/decorators.py
+++ helpers/decorators.py
@@ -6,6 +6,9 @@ from django.http import JsonResponse
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 
+from authorization.logics.otp import check_access_phone_number_to_otp, check_max_otp_request_and_block
+from authorization.models import OTP
+
 
 def login_required(f):
     @wraps(f)
@@ -24,3 +27,29 @@ def login_required(f):
             return JsonResponse(data={"message": "token is invalid"}, status=401)
         return f(request, *args, **kwargs)
     return wrapped
+
+
+def otp_firewall(**options):
+    def _login_required(f):
+        @wraps(f)
+        def wrapper(request, *args, **kwargs):
+            try:
+                data = OTP(strict=True).load(request.get_json() or {})[0] or {}
+                if check_access_phone_number_to_otp(data["phone"]):
+                    if not check_max_otp_request_and_block(data["phone"]):
+                        print("max otp request")
+                        return JsonResponse(data={"message": "max otp request"}, status=401)
+                else:
+                    print("check acess phone number")
+                    return JsonResponse(data={"message": "max otp request"}, status=401)
+            except ForbiddenError as ex:
+                print(ex)
+                raise ex
+            except HTTPException as ex:
+                print(ex)
+                raise ex
+            except:
+                return JsonResponse(data={"message": "max otp request"}, status=401)
+            return f(request, *args, **kwargs)
+        return wrapper
+    return _login_required
