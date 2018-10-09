19a05d43f45163179bffe1a6fd782443ca20fb30
Reza Noori <rezaoptic@yahoo.com>
2018-10-09 13:08:45 +0330

    user otp_firewall and better error return

--- authorization/views.py
+++ authorization/views.py
@@ -2,17 +2,18 @@ import json
 from django.views.decorators.http import require_http_methods
 from django.http import JsonResponse
 from authorization.logics.otp import set_otp, validate_otp
-from helpers.decorators import login_required
+from helpers.decorators import login_required, otp_firewall
 from helpers.exceptions import AuthorizationError
 from .models import OTP, OTPVerify
 
 
 @require_http_methods(["POST"])
+@otp_firewall()
 def send_otp(request):
     args = OTP(strict=True).load(json.loads(request.body or {}))[0]
     if set_otp(args["phone_number"]):
-        return JsonResponse(status=204, data={})
-    return JsonResponse(data={"message": "internal error"}, status=400)
+        return JsonResponse(status=204, data=None, safe=False)
+    raise AuthorizationError("Internal Service Error")
 
 
 @require_http_methods(["POST"])
